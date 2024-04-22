0bf99e34225e3b6eb3f93f6e8d19fedd168b7eef
jeff48872 <112918697+jeff48872@users.noreply.github.com>
2024-04-22 16:41:44 +0800

    week10

---
+++ week10-1_trt/main.cpp
@@ -0,0 +1,39 @@
+#include <GL/glut.h>
+void mybody(){
+    glColor3f(1,0,0);
+    glutWireCube(0.6);
+}
+
+void myArm(){
+    glColor3f(0,1,0);
+    glScalef(1.5,0.5,0.5);
+    glutWireCube(0.3);
+}
+float angle=0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        //glTranslatef(-0.6,+0.3,0);
+        mybody();
+    glPopMatrix();
+    glPushMatrix();
+        //glTranslatef(+0.0,+0.3,0);
+        glRotatef(angle++,0,0,1);
+        glTranslatef(0.225,0,0);
+        myArm();
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week10-3");
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutMainLoop();
+}
